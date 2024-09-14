af61598b28ae6b124bf837a4345200c8709a6825
BrevetEstMoi <144062355+gansirElmejor@users.noreply.github.com>
2024-09-13 20:50:57 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,64 @@
+//函数区间求解算法
+import java.util.ArrayList;
+class SetUps {
+    //配置函数的上下区间 E.G [-100,100]
+    private double UPPER_BOUND = 100;
+    private double LOWER_BOUND = -100;
+    private double PRECISION = 0.001; //精确度，解精确到小数点后第几位.
+    public double funn(double x) {
+        //配置函数解析式 E.G y=x^2 - 10
+        double y = Math.pow(x, 5) + Math.pow(x, 2);
+        return y;
+    }
+
+    public double getB() {return this.UPPER_BOUND;}
+    public double getA() {return this.LOWER_BOUND;}
+    public double getPrecision() {return this.PRECISION;}
+}
+public class Main {
+    public static void main(String[] args) {
+        SetUps stup = new SetUps();
+        Solver solver = new Solver(stup);
+        solver.solve();
+        ArrayList<Double> result = solver.getResult();
+        if (result.size()== 0) {
+            System.out.println("没有实数根 | No Real Root!");
+        };
+        for (int i=0;i<result.size();i++) {
+            System.out.println("x="+String.valueOf(result.get(i)));
+        }
+    }
+}
+class Solver{
+    SetUps stup;
+    double PRECISION;
+    ArrayList<Double> zeros;
+    Solver(SetUps stup){
+        this.stup = stup;
+        this.PRECISION = stup.getPrecision();
+        this.zeros = new ArrayList<Double>();
+    }
+    public void solve() {
+        recSolve(stup.getA(),stup.getB());
+    }
+    public ArrayList<Double> getResult() {
+        return this.zeros;
+    }
+    private void recSolve(double a, double b) {
+        double mean = (b + a)/2; // 求平均数
+        if (Math.abs((b-a))<PRECISION) {
+            zeros.add(mean);
+            return;
+        } else if (stup.funn(a)*stup.funn(mean) <= 0) {
+            recSolve(a,mean);
+            if (stup.funn(b)*stup.funn(mean) <= 0) { //Return Stack 的时候检查下二分的另一半行不行
+                recSolve(mean,b); 
+            } 
+        } else if (stup.funn(b)*stup.funn(mean) <= 0) {
+            recSolve(mean,b);
+            if (stup.funn(a)*stup.funn(mean) <= 0) {
+                recSolve(a,mean);
+            }            
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,30 @@
+# Demo: Function Solving Algorithm | 函数求零算法
+![Author](https://img.shields.io/badge/Author-gansir-green)  ![language](https://img.shields.io/badge/OpenJDK-1.8.x-green)
+### 0x01 Introduction | 简介：
+
+```
+Being devastated by solving polynomials? Try this algorithm!
+还在为解不出高次方程烦恼吗？快来学习这个算法吧！
+```
+
+**Feature|特点**
+
+* 1.Based on Math theorem, easy to understand. 基于数学定理，安全可靠。
+* 2.Acceptable time complexity. 时间复杂度不算太高。
+* 3.Polymorphic structured. 贯彻Polymorphism。
+* 4.Short and Simple. KISS. (keep it short and simple)
+
+
+
+### 0x02 How To Use | 使用：
+-require to have a java compiler: openjdk version "1.8.x" or higher
+-Inspect the code, configure your function in the class SetUps
+-configure the upper and lower bounds as well
+-啥都甭说，开java跑就完了！
+
+**DEMO|展示**
+```bash
+x=-0.9998321533203125
+x=-3.814697265625E-4
+x=3.814697265625E-4
+```
